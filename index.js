aa4e9b93cbaf87fe0e45adca807a1de1300060fc
hduss <maintenancedotnet3@gmail.com>
2021-07-10 00:41:24 +0200

    add const getAllDatas

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 const path = require('path')
+var mkdirp = require("mkdirp");
+
 const baseUrl = "http://laceliah.cowblog.fr/"
 
 
 process.setMaxListeners(Infinity)
+
+
+
 
 
 
@@ -30,54 +35,149 @@ const getDataFromUrl = async url => {
 	console.log('page ' + url + ' est chargée');
 
 
-	const titles = await page.$$('.article-top > .left');
-	for (let i = 0; i < titles.length; i++) {
-		const title = await (await titles[i].getProperty('innerText')).jsonValue()
-		// console.log(title)
-	}
+	// const titles = await page.$$('.article-top > .left');
+	// for (let i = 0; i < titles.length; i++) {
+	// 	const title = await (await titles[i].getProperty('innerText')).jsonValue()
+	// 	 console.log(title)
+	// }
 
-	const bodys = await page.$$('.article-body')
-	for (let i = 0; i < bodys.length; i++) {
-		const body = await (await bodys[i].getProperty('innerText')).jsonValue()
-		// console.log(body)
-	}
+	// let titles = await page.evaluate(() => {
+	// 	Array.from(document.querySelectorAll('.article-top > .left').map(el => el.innerText))
+	// })
 
-	let imagefiles = await page.evaluate(() =>
+	/*const articles = await page.$$eval('.article', articles => articles.map(article => article.getAttribute('id')
+	))*/
+
+	const articles = await page.$$eval('.article', articles => articles.map(article => {
+
+
+		let attr = article.getAttribute('id')
+		const titles = page.$$eval('.article-top > .left', titles => titles.map(title => {
+
+		return {
+			attr : title.textContent
+		}
+	}))
+
+	}))
+
+
+	console.log('articles => ', articles)
+
+
+	const titles = await page.$$eval('.article-top > .left', titles => titles.map(title => {
+
+		return {
+		'title' : title.textContent
+		}
+	}));
+
+
+	console.log('titles => ', titles)
+
+	const bodys = await page.$$eval('.article-body', bodys => bodys.map(el => el.textContent))
+	const imagefiles = await page.evaluate(() =>
 	    Array.from(
 	      document.querySelectorAll('#main img'),
 	      img => img.src
 	    )
 	)
 
-	console.log('Images => ', imagefiles)
 
 
-	
+	// Gestion de création des dossier/fichiers
+	let existingPath = await path.resolve('uploads/img')
+	if(!fs.existsSync(existingPath)){
+		mkdirp(existingPath, function(err) {
+		console.log(err)
+		});
 
-	/*const text = await page.evaluate(() => {
-		Array.from(document.querySelectorAll('.article-top'), element => element.textContent)
-	});*/
+	}else{
+		console.log('Exist path')
+	}
 
-
-
-/*	const eval = await page.evaluate(() => {
-		let title = document.querySelector('.article-top').innerText
-
-		
-
-		console.log('Ici le nom de l\'article => ', title)
-
-		return text
-	})*/
 
 	console.log('<===================' + url + '========================>')
 
 	browser.close()
-	return titles
+	return {titles, imagefiles}
 
 
 }
 
+
+
+
+const getallDatas = async url => {
+
+	console.log('getDataFromUrl url => ', url)
+
+	// 1 - Créer une instance de navigateur
+	// const browser = await puppeteer.launch({ headless: false })
+	const browser = await puppeteer.launch()
+	const page = await browser.newPage()
+
+	// Config du timeout
+    await page.setDefaultNavigationTimeout(0);
+	await page.goto(url)
+	// await page.setViewport({ width: 1000, height: 500 })
+	// Faire une pause d'une seconde
+	await page.waitFor(1000) 
+	//await page.waitForNavigation()
+
+
+
+	
+
+	const articles = await page.evaluate(() => {
+
+		let articles = []
+		let elements = document.querySelectorAll('div.article')
+		for(element of elements){
+
+			let title = element.querySelector('div.article-top > div.left')
+			let body = element.querySelector('div.article-body')
+			// let images = element.querySelectorAll('div.article-body img', img => img.src)
+
+			
+		    let imagefiles = Array.from(
+		      element.querySelectorAll('div.article-body img'),
+		      img => img.src
+    )
+
+
+			
+
+			articles.push({
+				title : title.textContent,
+				//body : body.textContent
+				images : imagefiles
+				
+			})
+
+			
+			// for(image of images){
+
+			// 	articles.images.push({image})
+			// }
+			
+			
+
+			
+		}
+		return articles
+	})
+
+	console.log('articles => ', articles)
+
+	browser.close()
+}
+
+
+// Créer les fichiers pour chaque article
+const createFolder = async path => {
+	return true
+}
 // Telecharge les fichiers dans un dossier
 const downloadDatas = async datas => {
 	let download = false
@@ -89,7 +189,7 @@ const downloadDatas = async datas => {
 const getAllUrls = async baseUrl => {
 
 	const urlList = [];
-	for( let i = 1; i < 2; i++){
+	for( let i = 2; i < 3; i++){
 		urlList.push(baseUrl + i + ".html");
 	}
 	return urlList;
@@ -100,7 +200,7 @@ const scrap = async() => {
 
 	const urlList = await getAllUrls(baseUrl)
 	const results = await Promise.all(
-		urlList.map(url => getDataFromUrl(url)),
+		urlList.map(url => getallDatas(url)),
 	)
 
 
@@ -113,8 +213,8 @@ const scrap = async() => {
 
 //const urls = getAllUrls(url)
 
-scrap().then( value => {
-	// console.log(value)
+scrap().then( results => {
+	console.log(results)
 	console.log('fin script')
 }).catch(error => {
 	console.log(error)
